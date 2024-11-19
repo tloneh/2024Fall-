4b1d2bf858c971c12eb1ce5e654b6e4a2426a6b9
geyao hu <153108732+tloneh@users.noreply.github.com>
2024-11-19 15:36:10 +0800

    Update main.py

--- assignment 2/main.py
+++ assignment 2/main.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from Softmax.Softmax import train_softmax
-
+from MLP.MLP import MLPmodel
 def load_data(path):
   
 def main:
-    
+    hidden_layers = 2
+    units = 128
+    mlp_model = MLPModel(hidden_layers=hidden_layers, units=units)
 if __name__ == '__main__':
     main()
