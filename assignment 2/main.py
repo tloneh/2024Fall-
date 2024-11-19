800c6a33323020e66c322e13e99ca958a8576060
geyao hu <153108732+tloneh@users.noreply.github.com>
2024-11-19 15:24:49 +0800

    Create main.py

---
+++ assignment 2/main.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from Softmax.Softmax import train_softmax
+
+def load_data(path):
+  
+def main:
+    
+if __name__ == '__main__':
+    main()
